25220566d38c2d81e01737ac04f5e29f6d74212b
Hanhai <85443834+HanhaiNotHai@users.noreply.github.com>
2023-01-05 04:08:25 +0800

    Feature (#6)
    * 更改示例
    
    * 更新.gitignore
    
    * 更新示例
    
    * 更新.gitignore
    
    * 更新.gitignore
    
    * 更新课表示例
    
    * 更新.gitignore
    
    * TimetableWithoutCourses.ipynb可以生成无课表
    todo: 创业基础一节课多个时间
    
    * 使用说明

--- TimetableWithoutCourses.py
+++ TimetableWithoutCourses.py
@@ -1,1 +1,19 @@
-print('TimetableWithoutCourses')
+import pdfplumber
+
+
+with pdfplumber.open('') as pdf:
+    tables = [page.extract_table() for page in pdf.pages]
+    for i in range(len(tables) - 1):
+        if (tables[i][-1][1] == '' or tables[i+1][0][1] == ''):
+            for col in range(len(tables[i+1][0])):
+                if (tables[i+1][0][col] != ''):
+                    rowOff = 0
+                    while tables[i][-1-rowOff][col] == None:
+                        rowOff += 1
+                    tables[i][-1-rowOff][col] += tables[i+1][0][col]
+            tables[i+1] = tables[i+1][1:]
+    table = []
+    for t in tables:
+        table += t
+    for row in table:
+        print(row)
